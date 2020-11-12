c16d347f870f01fa14a5d44ddf1e3a96a710cbee
Najib Ishaq <najib_ishaq@uri.edu>
2020-11-12 14:32:48 -0500

    using streamlit and drawing time-series plots

--- difference_equations/ode_solver.py
+++ difference_equations/ode_solver.py
@@ -1,50 +1,79 @@
-from typing import Tuple
+from typing import Tuple, List
 
 import numpy as np
+import streamlit as st
+from matplotlib import pyplot as plt
 from scipy.integrate import odeint
 
-from matplotlib import pyplot as plt
-
-R = 1.0
-K = 100
-E = 0.2
-C = 0.1
-D = 0.05
+from app import draw_phase_plot, get_isoclines
 
 
-def solve(initial: Tuple[float, float], time: np.array) -> np.array:
+def draw_time_series(populations: List[np.array], times: np.array):
+    plt.clf()
+    fig = plt.figure(figsize=(8, 5), dpi=128)
+    fig.add_subplot(111)
 
-    def differential(populations: np.array, _) -> Tuple[float, float]:
-        [num_pogies, num_stripers] = list(populations)
+    for run in populations:
+        plt.plot(times, run[0], c='blue', label='Prey Population', lw=0.75)
+        plt.plot(times, run[1], c='red', label='Predator Population', lw=0.75)
 
-        pogies_delta = num_pogies * (R * (1 - num_pogies / K) - C * num_stripers)
-        pogies_delta = max(pogies_delta, 3 - num_pogies)
-
-        stripers_delta = num_stripers * (E * C * num_pogies - D * num_stripers)
-        stripers_delta = max(stripers_delta, 1 - num_stripers)
-
-        return pogies_delta, stripers_delta
-
-    return odeint(differential, initial, time)
+    plt.xlabel('Time')
+    plt.ylabel('Populations')
+    plt.title('Prey (Blue) and Predators (red) vs time')
+    st.pyplot(fig)
+    return
 
 
 def main():
-    initial = 3, 1
-    delta_t = 4
-    time = [t / 4 for t in range(100 * delta_t + 1)]
-    populations = np.asarray(solve(initial, time))
+    st.title('Predator-Prey Differential Equations')
 
-    fig = plt.figure(figsize=(8, 5), dpi=200)
-    fig.add_subplot(111)
+    prey_capacity = st.slider('Prey Capacity', 50, 200, 100, 5)
+    col1, col2 = st.beta_columns(2)
+    with col1:
+        reproduction_rate = st.slider('Prey Reproduction Rate', 0., 1., 0.5, 0.05, '%.2f')
+        efficiency = st.slider('Ecological Efficiency', 0.05, 0.4, 0.15, 0.01, '%.2f')
+    with col2:
+        consumption_rate = st.slider('Consumption Rate', 0.01, 0.2, 0.05, 0.01, '%.2f')
+        death_rate = st.slider('Predator Death Rate', 0.01, 0.2, 0.05, 0.01, '%.2f')
+    grid_size = st.slider('Starting Grid', 0, 20, 0, 2)
+    grid_size = grid_size // 2
 
-    plt.plot(time, populations, lw=0.75)
+    min_prey, min_predators = 3, 1
 
-    plt.xlabel('Time (s)')
-    plt.ylabel('Populations')
-    plt.legend(['pogies', 'stripers'])
-    plt.title('Population vs Time of stripers and pogies from odeint')
-    plt.savefig('odeint_solution.png', bbox_inches='tight', pad_inches=0.25)
-    plt.close(fig)
+    def solve(initial: Tuple[float, float], time: np.array) -> np.array:
+
+        def differential(_p: np.array, _) -> Tuple[float, float]:
+            [num_pogies, num_stripers] = list(_p)
+
+            pogies_delta = num_pogies * (reproduction_rate * (1 - num_pogies / prey_capacity) - consumption_rate * num_stripers)
+            pogies_delta = max(pogies_delta, 3 - num_pogies)
+
+            stripers_delta = num_stripers * (efficiency * consumption_rate * num_pogies - death_rate * num_stripers)
+            stripers_delta = max(stripers_delta, 1 - num_stripers)
+            return pogies_delta, stripers_delta
+
+        return np.asarray(odeint(differential, initial, time)).T
+
+    time_steps = np.arange(start=0, stop=100, step=0.25)
+    first_run = solve((3, 1), time_steps)
+    populations: List[np.array] = [first_run]
+
+    if grid_size > 0:
+        max_prey, max_predators = max(first_run[0]), max(first_run[1])
+        prey_step, predators_step = (max_prey - min_prey) / grid_size, (max_predators - min_predators) / grid_size
+        for x in range(1, 1 + grid_size):
+            for y in range(1, 1 + grid_size):
+                starting_prey = min_prey + prey_step * (x - 0.5 * (y % 2))
+                starting_predators = min_predators + predators_step * (y + 0.5 * (x % 2))
+                new_run = solve((starting_prey, starting_predators), time_steps)
+                populations.append(new_run)
+
+    constants = [reproduction_rate, prey_capacity, consumption_rate, efficiency, death_rate]
+    isoclines = get_isoclines((min(first_run[0]), max(first_run[0])), *constants)
+    st.write(f'The Equilibrium populations are: prey: {first_run[0][-1]:.1f}, predators: {first_run[1][-1]:.1f}')
+
+    draw_time_series(populations, time_steps)
+    draw_phase_plot(populations, isoclines)
     return
 
 
